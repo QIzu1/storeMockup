a44cbf9a0e4c0393a276fd9754e8e8d9e392f11d
Qizu <51144762+QIzu1@users.noreply.github.com>
2023-03-27 21:04:26 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,136 @@
+<main>
+	<img src="/logo.png" alt="LOTC Logo">
+  
+	<div id="navbar">
+	  <a href="./home.html">Home</a>
+	  <a href="./store.html">Store</a>
+	  <a href="./account.html">Account</a>
+	</div>
+  
+	<div class="test-panel">
+		<div class="header">
+			<h2>Welcome to the Lord of the Craft Store Page!</h2>
+		</div>
+	  
+	  <div class="inner-panel">
+		<p>Sale Information</p>
+	  </div>
+	</div>
+	
+	<div style="height: 20px;"></div>
+	
+	<div class="top-contributor-panel">
+	  <div class="top-contributor-header">
+		<h3>Top Contributor</h3>
+	  </div>
+	  <div class="inner2-panel">
+		<p>
+		  <img src="/body.png" alt="Qizu">
+		  <span style="text-decoration: underline;">Qizu</span>
+		</p>
+	  </div>
+	</div>
+
+	
+
+  
+  </main>
+  
+  <style>
+
+	.top-contributor-panel {
+	  background-color: #c5bdbd;
+	  text-align: center;
+	  margin: 20px auto;
+	  max-width: 30%;
+	  height: 150px;
+	
+	}
+  
+	.top-contributor-header {
+	  background-color: #333;
+	  color: #fff;
+	  padding: 5px;
+	  text-align: center;
+	}
+
+	.header {
+	  background-color: #333;
+	  color: #fff;
+	  padding: 5px;
+	  text-align: center;
+	}
+  
+	.top-contributor-panel .inner2-panel {
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	  height: 55%;
+	}
+  
+	.top-contributor-panel p {
+	  margin: 0;
+	  font-size: 20px;
+	  color: rgb(104, 97, 97);
+	}
+  
+	.top-contributor-panel img {
+	  height: 50px;
+	  width: auto;
+	  margin-right: 10px;
+	  float: left;
+	}
+  
+	.test-panel {
+	  background-color: #333;
+	  text-align: center;
+	  margin: 20px auto;
+	  max-width: 30%;
+	  height: 300px;
+	  margin-bottom: 15px;
+	}
+  
+	.test-panel h2 {
+	  color: #fff;
+	  text-align: center;
+	  font-size: 20px;
+	  padding: 5px;
+	}
+  
+	.inner-panel {
+	  background-color: #c5bdbd;
+	  height: 85%;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	}
+  
+	.inner-panel p {
+	  color: rgb(104, 97, 97);
+	  text-align: center;
+	  font-size: 15px;
+	  margin: 15px;
+	}
+  
+	#navbar {
+	  overflow: hidden;
+	  background-color: #333;
+	  opacity: 0.6;
+	  margin-bottom: 20px;
+	  text-align: center;
+	}
+  
+	#navbar a {
+	  display: inline-block;
+	  color: #f2f2f2;
+	  text-align: center;
+	  padding: 14px 35px;
+	  text-decoration: none;
+	  font-size: 17px;
+	}
+  
+	#navbar a:hover {
+	  background-color: #ddd;
+	  color: black;
+	}
+  </style>
